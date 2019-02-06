da20589fdf2259b2ce692f7a5496c5b34cb5c040
Puppetmaster909 <pringthomas4@gmail.com>
2019-02-06 03:13:50 -0500

    Patrolling and Terrain
    Patrolling AI and terrain implemented for testing purposes

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -6,10 +6,23 @@ using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
-    [SerializeField]
-    Transform destination;
+    public Transform destination;
+
+    // Dictates whether the guard waits on each node
+    public bool patrolWaiting;
+
+    public float totalWaitTime = 3f;
+
+    public float switchProbability = 0.2f;
+
+    public List<Waypoint> patrolPoints;
 
     NavMeshAgent navMeshAgent;
+    int currentPatrolIndex;
+    bool traveling;
+    bool waiting;
+    bool patrolForward;
+    float waitTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -23,23 +36,81 @@ public class Patrol : MonoBehaviour
         }
         else
         {
-            SetDestination();
-        }
-    }
-
-    
-    private void SetDestination()
-    {
-        if (destination != null)
-        {
-            Vector3 targetVector = destination.transform.position;
-            navMeshAgent.SetDestination(targetVector);
+            if (patrolPoints != null && patrolPoints.Count >= 2)
+            {
+                currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("Need more patrol points");
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if close to destination
+        if (traveling && navMeshAgent.remainingDistance <= 1.0f)
+        {
+            traveling = false;
+
+            // If waiting active, then wait
+            if (patrolWaiting)
+            {
+                waiting = true;
+                waitTimer = 0;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        // Instead if we're waiting
+        if (waiting)
+        {
+            waitTimer += Time.deltaTime;
+            if (waitTimer >= totalWaitTime)
+            {
+                waiting = false;
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
     }
+
+
+    private void SetDestination()
+    {
+        if (destination != null)
+        {
+            Vector3 targetVector = patrolPoints[currentPatrolIndex].transform.position;
+            navMeshAgent.SetDestination(targetVector);
+            traveling = true;
+        }
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if (UnityEngine.Random.Range(0f, 1f) <= switchProbability)
+        {
+            patrolForward = !patrolForward;
+        }
+
+        if (patrolForward)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Count;
+        }
+        else
+        {
+            if (--currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolPoints.Count - 1;
+            }
+        }
+    }
+
 }
